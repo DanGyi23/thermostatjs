2561468e465b6c49f68c4136545947f89078015f
Dan Gyi <daniel.gyi@gmail.com>
2019-09-18 16:59:59 +0100

    add conditional css formatting for energy usage

--- jquery_body.js
+++ jquery_body.js
@@ -1,26 +1,43 @@
 $(document).ready(function () {
   var thermostat = new Thermostat;
+
+// UsageText displays different colour for energy consumption based on conditional
+  UsageText = function(text) {
+    $('#energyconsumption').text("Energy usage: " + thermostat.energyUsage())
+    if (text == "Energy usage: High-usage") {
+      return $('#energyconsumption').css("color", "red")
+    }
+    else if (text == "Energy usage: Medium-usage") {
+      return $('#energyconsumption').css("color", "black")
+    }
+    else {
+      return $('#energyconsumption').css("color", "green")
+    }
+  };
+
+
+
   $('#temperature').text("Temperature: " + thermostat.temperature + " Degrees");
   $('#powersavingmode').text("Energy Mode: " + thermostat.mode)
   $('#powersavingmode').css("color", "green")
-  $('#energyconsumption').text("Energy usage: " + thermostat.energyUsage())
+  UsageText("Energy usage: " + thermostat.energyUsage())
 
   $('#temperature-down').click(function () {
     thermostat.downTemp(1);
     $('#temperature').text("Temperature: " + thermostat.temperature + " Degrees");
-    $('#energyconsumption').text("Energy usage: " + thermostat.energyUsage())
+    UsageText("Energy usage: " + thermostat.energyUsage())
   });
 
   $('#temperature-up').click(function () {
     thermostat.upTemp(1);
     $('#temperature').text("Temperature: " + thermostat.temperature + " Degrees");
-    $('#energyconsumption').text("Energy usage: " + thermostat.energyUsage())
+    UsageText("Energy usage: " + thermostat.energyUsage())
   });
 
   $('#temperature-reset').click(function () {
     thermostat.reset();
     $('#temperature').text("Temperature: " + thermostat.temperature + " Degrees");
-    $('#energyconsumption').text("Energy usage: " + thermostat.energyUsage())
+    UsageText("Energy usage: " + thermostat.energyUsage())
   });
 
   $('#powersaving-on').click(function () {
@@ -30,7 +47,7 @@ $(document).ready(function () {
     $('#temperature').text("Temperature: " + thermostat.temperature + " Degrees");
     $('#powersavingmode').text("Energy Mode: " + thermostat.mode)
     $('#powersavingmode').css("color", "green")
-    $('#energyconsumption').text("Energy usage: " + thermostat.energyUsage())
+    UsageText("Energy usage: " + thermostat.energyUsage())
   })
 
   $('#powersaving-off').click(function () {
